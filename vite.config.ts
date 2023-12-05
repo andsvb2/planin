3db096f9291f90c70fd1ee3c2c3f50c956ce4730
Anderson S. Vieira <svieira.anderson@gmail.com>
2023-12-05 12:14:20 -0300

    chore: inicia o projeto

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+  resolve: {
+    alias: {
+      '@css': '/src/assets/css',
+      '@img': '/src/assets/images',
+      '@comp': '/src/components',
+      '@pages': '/src/pages',
+      '@utils': '/src/utils',
+      '@services': '/src/services'
+    }
+  },
+
+  plugins: [react()],
+})
