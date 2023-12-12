12def35c2df19a9de22fdab130b4ec70b1754b29
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-12-12 14:32:48 -0300

    feat!: reformula a tela de login usando componentes MUI

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,217 @@
+// import React from "react";
+// // import "@css/Login.css";
+// import Grid from "@mui/material/Unstable_Grid2";
+// import sideImage from "@img/studying-background.png";
+// import planinLogo from "@img/logo.svg";
+// import botaoGLogin from "@img/botao-login-google.png";
+// import Box from "@mui/material/Box";
+// import Container from "@mui/material/Container";
+// import Typography from "@mui/material/Typography";
+//
+// const Login: React.FC = () => {
+//   // return (
+//   //   <div className="login-container">
+//   //     <img src="../src/assets/img/logo.svg" alt="Logo" className="logo" />
+//   //     <div className="login-content">
+//   //       <img
+//   //         src="../src/assets/img/studying-background.png"
+//   //         alt="Imagem Lateral"
+//   //         className="side-image"
+//   //       />
+//   //       <h1>
+//   //         Planeje suas atividades com <br></br>mais facilidade e eficiência
+//   //       </h1>
+//   //     </div>
+//   //   </div>
+//   // );
+//
+//   return (
+//     <Container component="main" maxWidth="md">
+//       <Box
+//         sx={{
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           height: "100vh",
+//           width: "100vw",
+//           backgroundColor: "white",
+//         }}
+//       >
+//         <Grid container spacing={3}>
+//           <Grid item xs={12} md={8}>
+//             <Box
+//               sx={{
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 justifyContent: "center",
+//                 alignItems: "center",
+//                 height: 768,
+//                 width: 768,
+//               }}
+//             >
+//               <img
+//                 src={sideImage}
+//                 alt="Imagem Lateral"
+//                 width={768}
+//                 height={768}
+//               />
+//             </Box>
+//           </Grid>
+//           <Grid item xs={12} md={4}>
+//             <Box
+//               sx={{
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 justifyContent: "space-between",
+//                 alignItems: "center",
+//                 height: 768,
+//                 width: 590,
+//               }}
+//             >
+//               <Box
+//                 component="img"
+//                 src={planinLogo}
+//                 alt="Logo"
+//                 sx={{
+//                   width: 440,
+//                   height: 138,
+//                   alignItems: "center",
+//                   justifyContent: "center",
+//                 }}
+//               />
+//               <Box
+//                 component="img"
+//                 src={botaoGLogin}
+//                 alt="Logo"
+//                 sx={{
+//                   width: 311,
+//                   height: 57,
+//                   alignItems: "center",
+//                   justifyContent: "center",
+//                 }}
+//               />
+//
+//               <Box
+//                 sx={{
+//                   width: "100%",
+//                   padding: 18,
+//                   display: "flex",
+//                   flexDirection: "column",
+//                   justifyContent: "center",
+//                   alignItems: "center",
+//                 }}
+//               >
+//                 <Typography
+//                   variant="h4"
+//                   style={{ color: "#2D8FEB", fontWeight: "bold" }}
+//                 >
+//                   Planeje as suas atividades com
+//                   <br />
+//                   mais facilidade e eficiência
+//                 </Typography>
+//               </Box>
+//             </Box>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//     </Container>
+//   );
+// };
+//
+// export default Login;
+
 import React from "react";
-import "@css/Login.css";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import sideImage from "@img/studying-background.png";
+import planinLogo from "@img/logo.svg";
+import botaoGLogin from "@img/botao-login-google.png";
 
 const Login: React.FC = () => {
   return (
-    <div className="login-container">
-      <img src="../src/assets/img/logo.svg" alt="Logo" className="logo" />
-      <div className="login-content">
-        <img
-          src="../src/assets/img//studying-background.png"
-          alt="Imagem Lateral"
-          className="side-image"
-        />
-        <h1>
-          Planeje suas atividades com <br></br>mais facilidade e eficiência
-        </h1>
-      </div>
-    </div>
+    <Container component="main" maxWidth="xl" disableGutters>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          backgroundColor: "white",
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                width: "100%",
+              }}
+            >
+              <img
+                src={sideImage}
+                alt="Imagem Lateral"
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "center",
+                height: "100%",
+                width: "100%",
+              }}
+            >
+              <Box
+                component="img"
+                src={planinLogo}
+                alt="Logo"
+                sx={{ width: "80%", maxWidth: 440, marginBottom: "1rem" }}
+              />
+              <Box
+                component="img"
+                src={botaoGLogin}
+                alt="Logo"
+                sx={{ width: "80%", maxWidth: 311, marginBottom: "1rem" }}
+              />
+              <Box
+                sx={{
+                  width: "100%",
+                  padding: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  style={{
+                    color: "#2D8FEB",
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  Planeje as suas atividades com <br /> mais facilidade e
+                  eficiência
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
