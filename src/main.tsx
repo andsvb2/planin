fc8ed18da4e29e101b84c65c178ed7e1db030959
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-12-11 22:25:27 -0300

    chore: adiciona fontes inter e lato

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/900.css";
+import "@fontsource/lato/700.css";
 //import App from './App.tsx'
 import "@css/index.css";
 import Login from "@pages/Login";
